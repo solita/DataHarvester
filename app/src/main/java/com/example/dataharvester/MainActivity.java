b8fcd4ed4bb620c5acdab48d11303b66d6d5413d
Eveliina Toivanen <eveliina.toivanen@tuni.fi>
2022-03-06 19:49:30 +0200

    Creating record button.

--- app/src/main/java/com/example/dataharvester/MainActivity.java
+++ app/src/main/java/com/example/dataharvester/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.dataharvester;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton btnRecord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRecord = (ImageButton) findViewById(R.id.btn_record);
+
+        btnRecord.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Toast.makeText(MainActivity.this, "Recording on.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
