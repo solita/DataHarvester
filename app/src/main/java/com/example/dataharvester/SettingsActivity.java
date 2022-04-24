5f84c995b355daafaa61c06a3937535f5ed09f61
sarqq <baba.sara@hotmail.com>
2022-04-24 14:45:34 +0300

    Added layout to AnalysisActivity, and functionality to display results from JSON string. Updated settings.

--- app/src/main/java/com/example/dataharvester/SettingsActivity.java
+++ app/src/main/java/com/example/dataharvester/SettingsActivity.java
@@ -21,16 +21,15 @@ import androidx.appcompat.widget.Toolbar;
 
 import org.w3c.dom.Text;
 
-public class SettingsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class SettingsActivity extends AppCompatActivity{
 
     //shared preferences file for saving user preferences
     private final String sharedPrefFile = "com.example.dataharvester";
     private SharedPreferences preferences;
 
     //for audio type
-    static int position = 0;
-    private String recordtype;
-    private Spinner spinner;
+    private Switch audioTypeSwitch;
+    private String audioType;
     private final String KEY_AUDIOTYPE = "audioType";
 
     //for apiurl
@@ -38,6 +37,7 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
     private TextView currentlyUsing;
     private String currentApiUrl;
     private Button saveButton;
+    private Button defaultButton;
     private final String KEY_APIURL = "apiUrl";
 
     //for local file saving
@@ -59,14 +59,22 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
         preferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
         loadSettings();
 
-        //creating spinner menu for audio types
-        spinner = (Spinner) findViewById(R.id.spinner);
-        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this,
-                R.array.recordTypes, android.R.layout.simple_spinner_item);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(arrayAdapter);
-        spinner.setOnItemSelectedListener(this);
-        spinner.setSelection(position);
+        //creating switch for audio types
+        //on:
+        audioTypeSwitch = (Switch) findViewById(R.id.audiotype_switch);
+        audioTypeSwitch.setChecked(audioType.equals("Stereo"));
+        audioTypeSwitch.setText(audioType.equals("Stereo") ? "Stereo" : "Mono");
+        audioTypeSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                audioType = audioType.equals("Mono") ? "Stereo" : "Mono";
+                audioTypeSwitch.setChecked(audioType.equals("Stereo"));
+                audioTypeSwitch.setText(audioType.equals("Stereo") ? "Stereo" : "Mono");
+                MainActivity.recordType = audioType;
+                saveSettings();
+            }
+        });
+
 
         //creating text field for url input
         apiUrlField = (EditText) findViewById(R.id.apiurl_value);
@@ -79,22 +87,35 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
             @Override
             public void onClick(View view) {
                 currentApiUrl = apiUrlField.getText().toString();
+                apiUrlField.setText("");
                 MainActivity.apiUrl = currentApiUrl;
                 currentlyUsing.setText(getString(R.string.current_api_url_value, currentApiUrl));
                 saveSettings();
             }
         });
 
+        //creating button for restoring default api url
+        defaultButton = (Button) findViewById(R.id.apiurl_button_default);
+        defaultButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                apiUrlField.setText(getString(R.string.default_api_url_value));
+            }
+        });
+
+
         //creating setting for switch
         //on: keep local files after download
         //off: don't
         localFilesSaveSwitch = (Switch) findViewById(R.id.files_setting_switch);
         localFilesSaveSwitch.setChecked(keepLocalFiles);
+        localFilesSaveSwitch.setText(keepLocalFiles ? "On": "Off");
         localFilesSaveSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 keepLocalFiles = localFilesSaveSwitch.isChecked();
                 localFilesSaveSwitch.setChecked(keepLocalFiles);
+                localFilesSaveSwitch.setText(keepLocalFiles ? "On" : "Off");
                 MainActivity.keepLocalFiles = keepLocalFiles;
                 saveSettings();
             }
@@ -127,24 +148,11 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        recordtype = adapterView.getItemAtPosition(i).toString();
-        MainActivity.recordType = recordtype;
-        position = i;
-        saveSettings();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-
-    }
-
     //save settings
     public void saveSettings(){
         SharedPreferences.Editor prefEditor = preferences.edit();
 
-        prefEditor.putString(KEY_AUDIOTYPE, recordtype);
+        prefEditor.putString(KEY_AUDIOTYPE, audioType);
         prefEditor.putString(KEY_APIURL, currentApiUrl);
         prefEditor.putBoolean(KEY_LOCALFILES, keepLocalFiles);
         prefEditor.apply();
@@ -152,9 +160,9 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
 
     //load settings, default values if none present
     public void loadSettings(){
-        recordtype = preferences.getString(KEY_AUDIOTYPE, "Mono");
-        MainActivity.recordType = recordtype;
-        currentApiUrl = preferences.getString(KEY_APIURL, "placeholder");
+        audioType = preferences.getString(KEY_AUDIOTYPE, "Mono");
+        MainActivity.recordType = audioType;
+        currentApiUrl = preferences.getString(KEY_APIURL, "https://racekernel.com/dataharvester/");
         MainActivity.apiUrl = currentApiUrl;
         keepLocalFiles = preferences.getBoolean(KEY_LOCALFILES, false);
         MainActivity.keepLocalFiles = keepLocalFiles;
